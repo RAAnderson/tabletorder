e58c43d38fc9882e3f8436543197001bb38f9bad
Rick Anderson <rickerk7@gmail.com>
2013-04-20 22:02:13 -0500

    adjusted the order schema again,  embeded MenuItem inside of each ticket

--- models/orders.js
+++ models/orders.js
@@ -11,18 +11,28 @@ var orderSchema = new Schema({
 	, tip : 		{ type: Number }
 	, status : 		{ type: Number }
 	, date: 		{ type: Date, default: Date.now }
-	, tickets : 		[ticketSchema]
+	, tickets : 	[ticketSchema]
 });
 
 
 /// Represents every food item orderd
 var ticketSchema = new Schema({
-	tableId : 			{ type: Number }
 	, ticketStatusId : 	{ type: Number }   //Used to show many status's 1-5...
 	, employeeId : 		{ type: String }
-	, menuItemId : 		{ type: String }
 	, customization : 	{ type: String }  //Used for typing in customizations to an order
 	, date: 			{ type: Date, default: Date.now }
+	, menuItem : {
+		menuItemId : 	{ type: String }
+		, menuCatId : 	{ type: Number }
+		, foodType : 	{ type: String, trim: true }
+		, title : 		{ type: String, trim: true }
+		, description : { type: String }
+		, price : 		{ type: Number }
+		, menuStatus : 	{ type: Number } 	// predefined codes that label status, ex: 1 = live, 4 = out of stock
+		, menuType : 	{ type: String, trim: true }  //breakfast lunch dinner specific items
+		, customSeason :{ type: String }	//Used for spring only, summer only type items
+		, ingredients : { type: String } 	//long list of ingredients
+	}
 });
 
 
